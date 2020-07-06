fe05b0a4aee95fdd805db4622be9fc04f44f1a58
luizfelipecirqueira <lfcs18ts@gmail.com>
2020-07-06 15:32:08 -0300

    Commit do meu projeto no curso de javascript
    Meu primeiro commit dos meus códigos em javascript

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var d = new Date();
+alert(d);
+/*var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1; //ou count++//
+}*/
+
+/*var idade = prompt("Qual a sua idade?");
+if(idade >=18){
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+}*/
+
+/*var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+console.log(fruta.cor);
+var nome = "Luiz Felipe";
+var idade = 26;
+var idade2 = 10;
+var frase = "Meu projeto javascript";
+//alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("projeto", "curso"));
+console.log(frase.toLocaleLowerCase());
+console.log(frase.toLocaleUpperCase());
+var lista = ["Maçã", "Pêra", "laranja"];
+lista.push("uva");
+console.log(lista);
+//lista.pop();
+console.log(lista.toString());*/
