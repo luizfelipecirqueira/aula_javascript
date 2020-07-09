80aa9ba783dab052f46abee5cbf8c8daff3473a2
luizfelipecirqueira <lfcs18ts@gmail.com>
2020-07-09 19:52:00 -0300

    Commit curso ES6
    Última atualização de arquivos js, html do curso de JavaScript avançado com EcmaScript.

--- js/main2.js
+++ js/main2.js
@@ -21,3 +21,25 @@ function Car(){
     this.foo = 'bar';
 }
 console.log(new Car());
+
+function mutiplica(a,b){
+    return a * b;
+}
+console.log(mutiplica(3,5));
+
+function randomNumber(){
+    console.log('Gerando números randômicos...');
+    return Math.random() * 10;
+}
+function multiplicar(a, b = randomNumber()){
+    return a * b;
+}
+console.log(multiplicar(5));
+
+function method1(){
+    console.log('method called');
+}
+var obj = {
+    method1
+};
+obj.method1();
